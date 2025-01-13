2606a91474e6279357d3510868206b20996957d0
Dylan McDonald <dylanjmcdonald@gmail.com>
2025-01-13 17:50:24 -0600

    simple landing page for now

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        main: "url('/bg@2x.png')",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
